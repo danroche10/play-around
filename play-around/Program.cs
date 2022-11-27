d2587a84f4459a982865ad7faf4382b06c48a4b9
danroche10 <danielroche10@gmail.com>
2022-11-27 12:59:17 +0000

    Delegate chaining

--- play-around/Program.cs
+++ play-around/Program.cs
@@ -1,18 +1,21 @@
-﻿delegate bool MeDelegate(int n);
+﻿delegate void MeDelegate();
 
 class MainClass
 {
     static void Main()
     {
-        int[] numbers = { 2, 7, 3, 9, 5, 7, 1, 8, 64, 1000 };
-        IEnumerable<int> result = RunNumbersThroughGauntlet(numbers, n => n < 5);
-        foreach(int n in result)
-            Console.WriteLine(n);
+        MeDelegate d = Foo;
+        d += Goo;
+        d += Sue;
+        d -= Foo;
+        d += Foo;
+        foreach(MeDelegate m in d.GetInvocationList())
+        {
+            Console.WriteLine(m.Target + ": " + m.Method);
+        }
+        d();
     }
-    static IEnumerable<int> RunNumbersThroughGauntlet(IEnumerable<int> numbers, MeDelegate gauntlet)
-    {
-        foreach(int number in numbers)
-            if (gauntlet(number))
-                yield return number;
-    }
+    static void Foo() { Console.WriteLine("Foo()"); }
+    static void Goo() { Console.WriteLine("Goo()"); }
+    static void Sue() { Console.WriteLine("Sue()"); }
 }
