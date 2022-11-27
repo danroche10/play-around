7762092cbda0c6e9ecb5e529ce765e5e9f8aef2d
danroche10 <danielroche10@gmail.com>
2022-11-27 14:07:28 +0000

    EventHandler and sender

--- play-around/AnotherFile.cs
+++ play-around/AnotherFile.cs
@@ -1,13 +1,1 @@
-﻿using System;
-
-class Cow
-{
-    public event Action mooing;
-
-    public void PushSleepingCow()
-    {
-        // .....
-        if (mooing != null)
-            mooing();
-    }
-}
+﻿
--- play-around/Program.cs
+++ play-around/Program.cs
@@ -1,10 +1,30 @@
-﻿class MainClass
+﻿class Cow
+{
+    public string Name { get; set; }
+    public event EventHandler Moo;
+
+    public void BeTippedOver()
+    {   
+        if (Moo != null)
+            Moo(this, EventArgs.Empty);
+    }
+}
+
+class MainClass
 {
     static void Main()
     {
-        Cow cow = new Cow();
-        cow.mooing += 
-            () => Console.WriteLine("Handler");
-        cow.PushSleepingCow();
+        Cow c1 = new Cow { Name = "Betsy" };
+        c1.Moo += giggle;
+        Cow c2 = new Cow { Name = "Georgy" };
+        c2.Moo += giggle;
+        Cow victim = new Random().Next() % 2 == 0 ? c1 : c2;
+        victim.BeTippedOver();
+    }
+
+    static void giggle(object sender, EventArgs e)
+    {
+        Cow c = sender as Cow;
+        Console.WriteLine("Giggle giggle... We made " + c.Name + " moo!");
     }
 }
