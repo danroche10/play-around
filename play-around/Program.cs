8640e53acaae90d97512f33553097b58d99a40e5
danroche10 <danielroche10@gmail.com>
2022-12-08 10:37:01 +0000

    Stack overflow

--- play-around/Program.cs
+++ play-around/Program.cs
@@ -4,28 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Fruit fruit = new Orange();
-            Console.WriteLine(fruit.GetColor());
-            fruit = new Apple();
-            Console.WriteLine(fruit.GetColor());
+            Program program = new Program();
+            program.foo();
         }
-    }
-    public abstract class Fruit
-    {
-        public abstract string GetColor();
-    }
-    public class Apple : Fruit
-    {
-        public override string GetColor()
+
+        public int foo()
         {
-            return "Red";
+            return foo();
         }
-    }
-    public class Orange : Fruit
-    {
-        public override string GetColor()
-        {
-            return "Orange";
-        }
+
     }
 }
