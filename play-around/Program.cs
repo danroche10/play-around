b1b3c6f80c2e61b6d8bf24e80e533f4c877d8995
danroche10 <danielroche10@gmail.com>
2023-02-04 12:14:06 +0000

    Stack

--- play-around/Program.cs
+++ play-around/Program.cs
@@ -1,26 +1,15 @@
-﻿using System.Collections.Immutable;
-using System.Diagnostics;
+﻿using System.Collections;
 
 class Program
 {
     static void Main()
     {
-        int[] testArray = new int[1000000];
-        List<int> testList = new List<int>();
-        for (int i = 0; i < 1000000; i++)
-        {
-            //testArray[i] = i;
-            testList.Add(i);  
-        }
-        DateTimeOffset timeBeforeArrayIteration = (DateTimeOffset)DateTime.UtcNow;
-        foreach (int i in testList)
-        {
-            Console.WriteLine(i);
-        }
-        DateTimeOffset timeAfterArrayIteration = (DateTimeOffset)DateTime.UtcNow;
-        Console.WriteLine(timeBeforeArrayIteration.ToString());
-        Console.WriteLine(timeAfterArrayIteration.ToString());
-        // 28 seconds for array
-    }
+        Stack stack = new Stack();
+        stack.Push(1);
+        stack.Push("2");
+        stack.Push(3);
 
+        int poppedValue = (int)stack.Pop();
+
+    }
 }
